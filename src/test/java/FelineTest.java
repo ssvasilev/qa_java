e7113602c909b244d3125c74b1301a16fa6b0028
Sergey Vasilyev <s.s.vasilev@gmail.com>
2023-01-06 19:24:47 +0300

    Покрыл класс Feline, исправил опечатку в Lion, отвязал зависимость.

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,43 @@
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Spy
+    private Feline feline;
+
+    @Test
+    public void eatMeatReturnsPredatorMeal() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn (List.of("Животные", "Птицы", "Рыба"));
+        Assert.assertEquals(feline.eatMeat(), List.of("Животные", "Птицы", "Рыба"));
+    }
+
+    @Test
+    public void getFamilyReturnsFelines() {
+        Feline feline = new Feline();
+        Assert.assertEquals(feline.getFamily(), "Кошачьи");
+    }
+
+    @Test
+    public void getKittensReturnOneCount(){
+        Feline feline = new Feline();
+        Assert.assertEquals(feline.getKittens(), 1);
+
+    }
+
+    @Test
+    public void  getKittensReturnRandomCount() {
+        Feline feline = new Feline();
+        int randomCount = (int) (Math.random() * 200); //Задаём случайное число котят
+        Assert.assertEquals(feline.getKittens(randomCount), randomCount); //Проверяем, что количество котят не изменилось.
+    }
+
+}
